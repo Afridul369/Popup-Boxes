130f96f1baeebb0033254f41e323d45e196a879e
Afridul369 <afridul.dev@gmail.com>
2024-10-15 13:11:09 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// ===================Confirm==========
+
+function abir (){
+    let amit = confirm('do you want to delete?');
+    if (amit) {
+        console.log('Deleted');
+    }
+    else{
+        console.log('Not Deleted');
+    }
+}
+abir()
+
+// ===================Confirm==========
+
+
+// =================Prompt================
+
+function welcome(){
+    let name = prompt('What Is Your Name?');
+    if (name=null || name ) {
+   console.log('match not found');
+
+    }
+}
+// welcome()
+
+// =================Prompt================
